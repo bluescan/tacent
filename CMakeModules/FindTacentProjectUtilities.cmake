6e13093684ccad94ccbb1bee001dcac5ead86339
Tristan <19311462+bluescan@users.noreply.github.com>
2022-06-28 02:00:26 -0700

    Removed cxx_std_17 now we're using 20.

--- CMakeModules/FindTacentProjectUtilities.cmake
+++ CMakeModules/FindTacentProjectUtilities.cmake
@@ -79,7 +79,6 @@ function(tacent_target_compile_features PROJNAME)
 	target_compile_features(
 		${PROJNAME}
 		PRIVATE
-			# cxx_std_17
 			cxx_std_20
 			)
 endfunction(tacent_target_compile_features)
