1f9d8775535694165f4796d345120b0afb9e1092
tgrimmer <19311462+bluescan@users.noreply.github.com>
2023-04-08 18:40:00 -0700

    Added etcpack to th pkm test folder.

--- UnitTests/TestData/Images/PKM/MakeTestImages.bat
+++ UnitTests/TestData/Images/PKM/MakeTestImages.bat
@@ -1,41 +1,44 @@
+rem etcpack.exe -c etc1 -s slow -f RGB TacentTestPattern.png TEMPETC1_RGB.ktx
+rem EXIT /B
+
 echo ETC1 PKM
-etcpack.exe -c etc1 -s slow -f RGB TacentTestPattern.png ETC1_RGB.pkm
-etcpack.exe -c etc1 -s slow -f RGB TacentTestPattern1281x721.png ETC1_RGB_1281x721.pkm
-etcpack.exe -c etc1 -s slow -f RGB TacentTestPattern1282x722.png ETC1_RGB_1282x722.pkm
-etcpack.exe -c etc1 -s slow -f RGB TacentTestPattern1283x723.png ETC1_RGB_1283x723.pkm
+rem etcpack.exe -c etc1 -s slow -f RGB TacentTestPattern.png ETC1_RGB.pkm
+rem etcpack.exe -c etc1 -s slow -f RGB TacentTestPattern1281x721.png ETC1_RGB_1281x721.pkm
+rem etcpack.exe -c etc1 -s slow -f RGB TacentTestPattern1282x722.png ETC1_RGB_1282x722.pkm
+rem etcpack.exe -c etc1 -s slow -f RGB TacentTestPattern1283x723.png ETC1_RGB_1283x723.pkm
 
 echo ETC2 PKM
-etcpack.exe -c etc2 -s slow -f R TacentTestPattern.png ETC2_R.pkm
-etcpack.exe -c etc2 -s slow -f R_signed TacentTestPattern.png ETC2_Rs.pkm
-etcpack.exe -c etc2 -s slow -f RG TacentTestPattern.png ETC2_RG.pkm
-etcpack.exe -c etc2 -s slow -f RG_signed TacentTestPattern.png ETC2_RGs.pkm
-etcpack.exe -c etc2 -s slow -f RGB TacentTestPattern.png ETC2_RGB.pkm
-etcpack.exe -c etc2 -s slow -f RGBA1 TacentTestPattern.png ETC2_RGBA1.pkm
-etcpack.exe -c etc2 -s slow -f RGBA8 TacentTestPattern.png ETC2_RGBA8.pkm
-etcpack.exe -c etc2 -s slow -f sRGB TacentTestPattern.png ETC2_sRGB.pkm
-etcpack.exe -c etc2 -s slow -f sRGBA1 TacentTestPattern.png ETC2_sRGBA1.pkm
-etcpack.exe -c etc2 -s slow -f sRGBA8 TacentTestPattern.png ETC2_sRGBA8.pkm
-etcpack.exe -c etc2 -s slow -f RGB TacentTestPattern1281x721.png ETC2_RGB_1281x721.pkm
-etcpack.exe -c etc2 -s slow -f RGB TacentTestPattern1282x722.png ETC2_RGB_1282x722.pkm
-etcpack.exe -c etc2 -s slow -f RGB TacentTestPattern1283x723.png ETC2_RGB_1283x723.pkm
+rem etcpack.exe -c etc2 -s slow -f R TacentTestPattern.png ETC2_R.pkm
+rem etcpack.exe -c etc2 -s slow -f R_signed TacentTestPattern.png ETC2_Rs.pkm
+rem etcpack.exe -c etc2 -s slow -f RG TacentTestPattern.png ETC2_RG.pkm
+rem etcpack.exe -c etc2 -s slow -f RG_signed TacentTestPattern.png ETC2_RGs.pkm
+rem etcpack.exe -c etc2 -s slow -f RGB TacentTestPattern.png ETC2_RGB.pkm
+rem etcpack.exe -c etc2 -s slow -f RGBA1 TacentTestPattern.png ETC2_RGBA1.pkm
+rem etcpack.exe -c etc2 -s slow -f RGBA8 TacentTestPattern.png ETC2_RGBA8.pkm
+rem etcpack.exe -c etc2 -s slow -f sRGB TacentTestPattern.png ETC2_sRGB.pkm
+rem etcpack.exe -c etc2 -s slow -f sRGBA1 TacentTestPattern.png ETC2_sRGBA1.pkm
+rem etcpack.exe -c etc2 -s slow -f sRGBA8 TacentTestPattern.png ETC2_sRGBA8.pkm
+rem etcpack.exe -c etc2 -s slow -f RGB TacentTestPattern1281x721.png ETC2_RGB_1281x721.pkm
+rem etcpack.exe -c etc2 -s slow -f RGB TacentTestPattern1282x722.png ETC2_RGB_1282x722.pkm
+rem etcpack.exe -c etc2 -s slow -f RGB TacentTestPattern1283x723.png ETC2_RGB_1283x723.pkm
 
 echo ETC1 KTX
-etcpack.exe -c etc1 -s slow -f RGB TacentTestPattern.png ETC1_RGB.ktx
-etcpack.exe -c etc1 -s slow -f RGB TacentTestPattern1281x721.png ETC1_RGB_1281x721.ktx
-etcpack.exe -c etc1 -s slow -f RGB TacentTestPattern1282x722.png ETC1_RGB_1282x722.ktx
-etcpack.exe -c etc1 -s slow -f RGB TacentTestPattern1283x723.png ETC1_RGB_1283x723.ktx
+rem etcpack.exe -c etc1 -s slow -f RGB TacentTestPattern.png ETC1_RGB.ktx
+rem etcpack.exe -c etc1 -s slow -f RGB TacentTestPattern1281x721.png ETC1_RGB_1281x721.ktx
+rem etcpack.exe -c etc1 -s slow -f RGB TacentTestPattern1282x722.png ETC1_RGB_1282x722.ktx
+rem etcpack.exe -c etc1 -s slow -f RGB TacentTestPattern1283x723.png ETC1_RGB_1283x723.ktx
 
 echo ETC2 KTX
-etcpack.exe -c etc2 -s slow -f R TacentTestPattern.png ETC2_R.ktx
-etcpack.exe -c etc2 -s slow -f R_signed TacentTestPattern.png ETC2_Rs.ktx
-etcpack.exe -c etc2 -s slow -f RG TacentTestPattern.png ETC2_RG.ktx
-etcpack.exe -c etc2 -s slow -f RG_signed TacentTestPattern.png ETC2_RGs.ktx
-etcpack.exe -c etc2 -s slow -f RGB TacentTestPattern.png ETC2_RGB.ktx
-etcpack.exe -c etc2 -s slow -f RGBA1 TacentTestPattern.png ETC2_RGBA1.ktx
-etcpack.exe -c etc2 -s slow -f RGBA8 TacentTestPattern.png ETC2_RGBA8.ktx
-etcpack.exe -c etc2 -s slow -f sRGB TacentTestPattern.png ETC2_sRGB.ktx
-etcpack.exe -c etc2 -s slow -f sRGBA1 TacentTestPattern.png ETC2_sRGBA1.ktx
-etcpack.exe -c etc2 -s slow -f sRGBA8 TacentTestPattern.png ETC2_sRGBA8.ktx
-etcpack.exe -c etc2 -s slow -f RGB TacentTestPattern1281x721.png ETC2_RGB_1281x721.ktx
-etcpack.exe -c etc2 -s slow -f RGB TacentTestPattern1282x722.png ETC2_RGB_1282x722.ktx
-etcpack.exe -c etc2 -s slow -f RGB TacentTestPattern1283x723.png ETC2_RGB_1283x723.ktx
+rem etcpack.exe -c etc2 -s slow -f R TacentTestPattern.png ETC2_R.ktx
+rem etcpack.exe -c etc2 -s slow -f R_signed TacentTestPattern.png ETC2_Rs.ktx
+rem etcpack.exe -c etc2 -s slow -f RG TacentTestPattern.png ETC2_RG.ktx
+rem etcpack.exe -c etc2 -s slow -f RG_signed TacentTestPattern.png ETC2_RGs.ktx
+rem etcpack.exe -c etc2 -s slow -f RGB TacentTestPattern.png ETC2_RGB.ktx
+rem etcpack.exe -c etc2 -s slow -f RGBA1 TacentTestPattern.png ETC2_RGBA1.ktx
+rem etcpack.exe -c etc2 -s slow -f RGBA8 TacentTestPattern.png ETC2_RGBA8.ktx
+rem etcpack.exe -c etc2 -s slow -f sRGB TacentTestPattern.png ETC2_sRGB.ktx
+rem etcpack.exe -c etc2 -s slow -f sRGBA1 TacentTestPattern.png ETC2_sRGBA1.ktx
+rem etcpack.exe -c etc2 -s slow -f sRGBA8 TacentTestPattern.png ETC2_sRGBA8.ktx
+rem etcpack.exe -c etc2 -s slow -f RGB TacentTestPattern1281x721.png ETC2_RGB_1281x721.ktx
+rem etcpack.exe -c etc2 -s slow -f RGB TacentTestPattern1282x722.png ETC2_RGB_1282x722.ktx
+rem etcpack.exe -c etc2 -s slow -f RGB TacentTestPattern1283x723.png ETC2_RGB_1283x723.ktx
