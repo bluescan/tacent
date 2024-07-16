f78da2f16fe9d698d608b126d791679e01d29e56
tgrimmer <19311462+bluescan@users.noreply.github.com>
2024-07-16 00:57:06 -0700

    Update LibKTX. Static library built from forked KTX-Software with astcenc set to use SSE2 instead of AVX.

--- Modules/Image/Contrib/LibKTX/include/version.h
+++ Modules/Image/Contrib/LibKTX/include/version.h
@@ -4,8 +4,8 @@
 v4.0
 // [API version]
 // [Code version]
-v4.3.2-26-g759f49d9-dirty
+v4.3.2~4^2-dirty
 // [Code version]
 */
-#define LIBKTX_VERSION v4.3.2-26-g759f49d9-dirty
+#define LIBKTX_VERSION v4.3.2~4^2-dirty
 #define LIBKTX_DEFAULT_VERSION v4.0.__default__
